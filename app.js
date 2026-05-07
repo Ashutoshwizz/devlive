a4569a762cceff10b9029da5858d0a0b0611cc6e
Ashutoshwizz <ashutoshwizz121@gmail.com>
2026-05-07 15:29:01 +0530

    Initial backend deployment

---
+++ app.js
@@ -0,0 +1,41 @@
+const express = require("express");
+const connectDB = require("./src/Config/database");
+const cookieParser = require("cookie-parser");
+const app = express();
+const dotenv = require("dotenv");
+dotenv.config({});
+const cors = require("cors");
+
+app.use(
+  cors({
+    origin:true,
+    credentials: true,
+  })
+);
+
+app.use(express.json());
+app.use(cookieParser());
+
+//routes
+const authRouter = require("./src/routes/auth");
+const profileRouter = require("./src/routes/profile");
+const requestRouter = require("./src/routes/request");
+const userRouter = require("./src/routes/user");
+
+app.use("/", authRouter);
+app.use("/", profileRouter);
+app.use("/", requestRouter);
+app.use("/", userRouter);
+
+//database connect before server
+connectDB().then(() => {
+  try {
+    const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log(`Server running on ${PORT}`);
+});
+  } catch (error) {
+    console.log(error);
+  }
+});
